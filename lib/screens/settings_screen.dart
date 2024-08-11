c5b7e3c4f4b596385023297934ac61307c4d0040
Andrew Marfo <andrewmarfo23@gmail.com>
2024-08-11 02:24:10 +0000

    removing unused imported package

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:twitter_clone/components/my_settings_list_tile.dart';
