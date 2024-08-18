f5453682df4d8d6612d3424e2ef5c43e8adbd5f1
Andrew Marfo <andrewmarfo23@gmail.com>
2024-08-18 00:55:23 +0000

    initialized firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:twitter_clone/firebase_options.dart';
 // import 'package:twitter_clone/screens/homepage.dart';
 import 'package:twitter_clone/screens/login_page.dart';
 import 'package:twitter_clone/services/auth/register_login.dart';
@@ -8,6 +10,8 @@ import 'package:twitter_clone/themes/theme_provider.dart';
 import 'screens/register_page.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized;
+  Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(
     ChangeNotifierProvider(
       create: (context) => ThemeProvider(),
