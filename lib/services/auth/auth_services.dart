f5eb1884db9123ba5c3bfbc9e1c01b3a622c209b
Andrew Marfo <andrewmarfo23@gmail.com>
2024-08-18 23:47:31 +0000

    create user accounts and sign in functions

--- lib/services/auth/auth_services.dart
+++ lib/services/auth/auth_services.dart
@@ -1,5 +1,30 @@
+import 'package:firebase_auth/firebase_auth.dart';
 
+class AuthServices {
+  final _auth = FirebaseAuth.instance;
+  User get user => _auth.currentUser!;
 
-class AuthServices{
+  // Create User account
+  createAccount(String email, String password) async {
+    try {
+      await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } catch (e) {
+      return e;
+    }
+  }
 
+  // Login User In
+  login(String email, String password) async {
+    try {
+      await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } catch (e) {
+      return e;
+    }
+  }
 }
