5424ef809fb0cb8b57d6b4281215710d323439cb
Andrew Marfo <andrewmarfo23@gmail.com>
2024-08-19 13:58:22 +0000

    user model

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class UserModel {
   final String uid;
   final String name;
@@ -12,4 +14,26 @@ class UserModel {
     required this.username,
     required this.bio,
   });
+
+  // convert firestore document to user profile to be  used in the app
+  factory UserModel.fromDocument(DocumentSnapshot doc) {
+    return UserModel(
+      uid: doc['uid'],
+      name: doc['name'],
+      email: doc['email'],
+      username: doc['username'],
+      bio: doc['bio'],
+    );
+  }
+
+  // convert a user profile to a map to be stored in firebase
+  Map<String, dynamic> toMap() {
+    return {
+      'uid': uid,
+      'name': name,
+      'email': email,
+      'username': username,
+      'bio': bio,
+    };
+  }
 }
