12f9704e7e9118cd86b389d4dd52a403a61c1385
Andrew Marfo <andrewmarfo23@gmail.com>
2024-08-19 13:11:30 +0000

    user model

---
+++ lib/models/user_model.dart
@@ -0,0 +1,15 @@
+class UserModel {
+  final String uid;
+  final String name;
+  final String email;
+  final String username;
+  final String bio;
+
+  UserModel({
+    required this.uid,
+    required this.name,
+    required this.email,
+    required this.username,
+    required this.bio,
+  });
+}
