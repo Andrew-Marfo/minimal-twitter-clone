93a3d7a5d01600721db3879711fc282c3508eae9
Andrew Marfo <andrewmarfo23@gmail.com>
2024-08-19 20:50:22 +0000

    database provider

---
+++ lib/services/database/database_provider.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:twitter_clone/models/user_model.dart';
+import 'package:twitter_clone/services/auth/auth_services.dart';
+import 'package:twitter_clone/services/database/database_services.dart';
+
+class DatabaseProvider extends ChangeNotifier {
+  final _auth = AuthServices();
+  final _db = DatabaseServices();
+
+  Future<UserProfile?> userProfile(String uid) {
+    return _db.getUserFromFirebase(uid);
+  }
+}
